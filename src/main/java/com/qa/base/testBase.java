4000751330d466e42c6a312db179a1789fced717
Murugesh <dharashanmurgesh@gmail.com>
2025-12-14 18:04:18 +0530

    Commiting New Work

--- src/main/java/com/qa/base/testBase.java
+++ src/main/java/com/qa/base/testBase.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,6 +18,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 public class testBase {
@@ -24,19 +27,23 @@ public class testBase {
 	 //Thread-safe WebDriver
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-	public static Properties p;
-	public  FileReader file;
+	private static Properties p;
+	private static  FileReader file;
+	
 	
 	
 	 //Getter method (IMPORTANT)
     public static WebDriver getDriver() {
         return driver.get();
     }
-
+    
+    protected void setDriver(WebDriver driverInstance) {
+        driver.set(driverInstance);
+    }
 
 	@BeforeMethod			
-	@Parameters({"os","browser"})
-	public void intailisation(String os, String br) throws IOException, Exception
+	@Parameters({"browser"})
+	public void intailisation(String br) throws IOException, Exception
 	{
 		URL gridURL = new URL("http://localhost:4444/wd/hub");
 		file = new FileReader("./src//test//resources//config.properties");
@@ -69,6 +76,7 @@ public class testBase {
 				edgeOptions.setCapability("se:name", "LoginTest_Edge");
 				edgeOptions.setCapability("se:recordVideo", true);
 				localDriver = new RemoteWebDriver(gridURL, edgeOptions);
+				break;
 			default:
                 throw new RuntimeException("Invalid browser name");
 
@@ -89,8 +97,7 @@ public class testBase {
 	     //Store driver in ThreadLocal
         driver.set(localDriver);
 		getDriver().manage().window().maximize();
-		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
+		getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 	}
 
 	@Test
@@ -99,7 +106,11 @@ public class testBase {
 		getDriver().get("https://opensource-demo.orangehrmlive.com/");
 		System.out.println(getDriver().getCurrentUrl());
 		assertEquals("OrangeHRM",getDriver().getTitle());
-		Thread.sleep(10000);
+		getDriver().findElement(By.name("username")).sendKeys("admin");
+		getDriver().findElement(By.name("password")).sendKeys("admin123");
+		WebDriverWait wait = new WebDriverWait(getDriver(),Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[normalize-space()='Login']")));
+		getDriver().findElement(By.xpath("//button[normalize-space()='Login']")).click();		
 	}
 
 	@BeforeSuite
@@ -114,7 +125,7 @@ public class testBase {
 		Thread.sleep(10000);
 	}
 
-	 @AfterMethod(alwaysRun = true)
+	 @AfterMethod()
 	    public void tearDown() {
 	        if (getDriver() != null) {
 	            getDriver().quit();     
@@ -123,7 +134,7 @@ public class testBase {
 	    }
 
 
-	@AfterSuite
+//	@AfterSuite
 	public void flush() throws Exception
 	{
 		Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
